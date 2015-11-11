9d00db359c0a4156f2f82b0c5084643d0f1ea8ff
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2015-11-10 18:20:01 -0700

    Update lf_rewrite_draft.sql

--- lf_rewrite_draft.sql
+++ lf_rewrite_draft.sql
@@ -1,1 +1,18 @@
+select sum(number), event_type_id
 
+from
+
+(select distinct extent.accession_id, extent.number, event_type_id
+
+from extent 
+
+join accession on accession.id = extent.accession_id
+
+left join event_link_rlshp /* one to many */
+		on accession.id = event_link_rlshp.accession_id
+
+left join event on event_link_rlshp.event_id = event.id
+
+where extent.extent_type_id = 278) as foo
+
+group by event_type_id

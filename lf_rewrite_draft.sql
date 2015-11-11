39230388588918ef0cd227e0e522af9881e8cfd3
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2015-11-10 18:26:28 -0700

    Update lf_rewrite_draft.sql

--- lf_rewrite_draft.sql
+++ lf_rewrite_draft.sql
@@ -1,5 +1,7 @@
 /* standardize your deacessioning or you might double-count deacessions */
 
+/* This works, BUT some records have 313 AND 1514 and are counting twice */
+
 select sum(number), event_type_id
 
 from
@@ -8,12 +10,14 @@ from
 
 from extent 
 
-join accession on accession.id = extent.accession_id
+join accession
+	on accession.id = extent.accession_id
 
-left join event_link_rlshp /* one to many */
-		on accession.id = event_link_rlshp.accession_id
+left join event_link_rlshp
+	on accession.id = event_link_rlshp.accession_id
 
-left join event on event_link_rlshp.event_id = event.id
+left join event
+	on event_link_rlshp.event_id = event.id
 
 where extent.extent_type_id = 278) as foo
 

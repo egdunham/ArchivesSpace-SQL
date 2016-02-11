93fead5ee9ddc84269a7d3e91debbc687a9c5a18
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2016-02-11 15:20:10 -0700

    Update linear_feet_on_aao.sql

--- linear_feet_on_aao.sql
+++ linear_feet_on_aao.sql
@@ -1,16 +1,30 @@
-/* Update to use events */
 select sum(extent.number)
 
-from archivesspace.accession 
-right join archivesspace.extent 
-on archivesspace.accession.id = archivesspace.extent.accession_id
+from
 
-left join archivesspace.user_defined 
-on archivesspace.accession.id = archivesspace.user_defined.accession_id
+/*Isolate all records that don't have a processing event and remove duplicates*/
+(select accession.id as master_id, accession.title as title
 
-left join archivesspace.collection_management
-on archivesspace.accession.id = archivesspace.collection_management.accession_id
+		from accession 
 
-where (text_4 = 'INV_AAO' or text_2 = 'INV_AAO')
-and extent.extent_type_id = 278
-and processing_status_id is null
+		left join event_link_rlshp
+			on accession.id = event_link_rlshp.accession_id
+	
+		left join event
+			on event_link_rlshp.event_id = event.id
+
+		where (event.event_type_id not in ('313', '1514')
+			or event.event_type_id is null)
+
+		group by accession.id, accession.title) as unprocessed
+
+	/*Isolate records with INV_AAO */
+
+		left join user_defined
+			on unprocessed.master_id = user_defined.accession_id
+
+		right join archivesspace.extent 
+			on unprocessed.master_id = extent.accession_id
+
+		where (text_4 = 'INV_AAO' or text_2 = 'INV_AAO')
+			and extent.extent_type_id = 278

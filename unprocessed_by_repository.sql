f9d0072e8384f43845275a684c5b7d13b44349c2
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2016-05-03 18:15:03 -0700

    Update unprocessed_by_repository.sql

--- unprocessed_by_repository.sql
+++ unprocessed_by_repository.sql
@@ -1,30 +1,50 @@
-/* Retool to use Events */
+select sum(unprocessed.lf), repository.name
 
-select sum(archivesspace.extent.number), repository.name
+	from (select distinct
+		accession.id as id, 
+		accession.title as title, 
+		accession.content_description as descr, 
+		accession.identifier as accno, 
+		accession.repo_id as repo_id, 
+		extent.number as lf, 
+		extent.extent_type_id as type
 
-from archivesspace.accession left join archivesspace.extent 
-	on accession.id = extent.accession_id
-
-	left join archivesspace.collection_management
-	on accession.id = collection_management.accession_id
-
-	left join archivesspace.deaccession
-	on accession.id = deaccession.accession_id
+		from accession
 	
-	left join archivesspace.user_defined
-	on accession.id = user_defined.accession_id
+		right join extent 
+			on accession.id = extent.accession_id
 
-	left join archivesspace.repository
-	on accession.repo_id = repository.id
+		left join event_link_rlshp
+			on accession.id = event_link_rlshp.accession_id
 
-	where extent.extent_type_id = 278
-	and (collection_management.processing_status_id is null 
-		or collection_management.processing_status_id != 257)
+		where not exists (select 1
+					from event_link_rlshp
+					left join event on event_link_rlshp.event_id = event.id
+					where accession.id = event_link_rlshp.accession_id 
+						and (event.event_type_id in ('313', '1514', '1515', '1512')))
+					) as unprocessed
 
-	and (user_defined.text_2 is null
-		or user_defined.text_2 != 'INV_AAO')
+		left join archivesspace.deaccession
+			on unprocessed.id = deaccession.accession_id
 
-	and (user_defined.text_4 is null
-		or user_defined.text_4 != 'INV_AAO')
+		left join archivesspace.user_defined
+			on unprocessed.id = user_defined.accession_id
 
-	group by repository.name
+		left join archivesspace.repository
+			on unprocessed.repo_id = repository.id
+
+		left join archivesspace.date
+			on unprocessed.id = date.accession_id
+
+		where unprocessed.type = 278
+
+		and (user_defined.text_2 is null
+			or user_defined.text_2 != 'INV_AAO')
+
+		and (user_defined.text_4 is null
+			or user_defined.text_4 != 'INV_AAO')
+
+		and (deaccession.scope_id is null 
+			or deaccession.scope_id = '923')
+
+group by repository.name

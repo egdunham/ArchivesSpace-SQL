3adb30d7c5c593e90932ea16d52441faf1b9a242
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2016-05-20 11:41:40 -0700

    Update processed_total.sql

--- processed_total.sql
+++ processed_total.sql
@@ -1,10 +1,32 @@
-select sum(archivesspace.extent.number)
+select sum(processed.lf) 
 
-from archivesspace.accession
-right join archivesspace.extent on accession.id = extent.accession_id
+from (select distinct
+		accession.id as id, 
+		accession.title as title, 
+		accession.content_description as descr, 
+		accession.identifier as accno, 
+		accession.repo_id as repo_id, 
+		extent.number as lf, 
+		extent.extent_type_id as type
 
-left join archivesspace.collection_management
-on accession.id = collection_management.accession_id
+		from accession
+	
+		right join extent 
+			on accession.id = extent.accession_id
 
-where collection_management.processing_status_id = 257 
-	and extent.extent_type_id = 278
+		left join event_link_rlshp
+			on accession.id = event_link_rlshp.accession_id
+
+		where exists (select 1
+					from event_link_rlshp
+					left join event on event_link_rlshp.event_id = event.id
+					where accession.id = event_link_rlshp.accession_id 
+						and (event.event_type_id in ('313', '1514')))) as processed
+
+		left join archivesspace.deaccession
+			on processed.id = deaccession.accession_id
+
+		where processed.type = 278
+		
+		and (deaccession.scope_id is null 
+			or deaccession.scope_id = '923')

533ef0b185488505cad2ee80f50b12e02057ed2d
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2016-08-24 15:23:35 -0700

    Update linear_feet_on_aao.sql

--- linear_feet_on_aao.sql
+++ linear_feet_on_aao.sql
@@ -1,30 +1,21 @@
 select sum(extent.number)
 
-from
-
-/*Isolate all records that don't have a processing event and remove duplicates*/
-(select accession.id as master_id, accession.title as title
-
-	from accession 
-
-	left join event_link_rlshp
-		on accession.id = event_link_rlshp.accession_id
-	
-	left join event
-		on event_link_rlshp.event_id = event.id
-
-	where (event.event_type_id not in ('313', '1514')
-		or event.event_type_id is null)
-
-	group by accession.id, accession.title) as unprocessed
-
-/*Isolate records with INV_AAO */
+from accession
 
 	left join user_defined
-		on unprocessed.master_id = user_defined.accession_id
+		on accession.id = user_defined.accession_id
 
 	right join archivesspace.extent 
-		on unprocessed.master_id = extent.accession_id
+		on accession.id = extent.accession_id
+
+		left join archivesspace.deaccession
+			on accession.id = deaccession.accession_id
 
 	where (text_4 = 'INV_AAO' or text_2 = 'INV_AAO')
+
 		and extent.extent_type_id = 278
+
+		and (deaccession.scope_id is null 
+			or deaccession.scope_id = '923')
+
+		and (accession.id <> 0 and accession.id is not null)

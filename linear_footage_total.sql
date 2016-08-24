9f1cad1886469e96ad6842758a8d1dd7424b1939
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2016-08-24 16:17:25 -0700

    Update linear_footage_total.sql

--- linear_footage_total.sql
+++ linear_footage_total.sql
@@ -1,14 +1,16 @@
-select sum(extent.number)
+select accession.id
 
-from archivesspace.accession
+from accession
 
 right join archivesspace.extent
-on archivesspace.accession.id = archivesspace.extent.accession_id
+on accession.id = extent.accession_id
 
 left join archivesspace.deaccession
-			on unprocessed.id = deaccession.accession_id
+			on accession.id = deaccession.accession_id
 
 where extent.extent_type_id = 278
 
 and (deaccession.scope_id is null 
 			or deaccession.scope_id = '923')
+
+and (accession.id <> 0 and accession.id is not null)

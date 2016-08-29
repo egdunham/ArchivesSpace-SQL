743bbec99a0ef8121dd44491614e4c1c8c0fb3a3
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2016-08-29 11:34:40 -0700

    Update processed_during.sql

--- processed_during.sql
+++ processed_during.sql
@@ -11,16 +11,19 @@ select
 from accession
 
 	join extent 
-		on accession.id = extent.accession_id and extent_type_id = 278
+			on accession.id = extent.accession_id and extent_type_id = 278
+
+	left join event_link_rlshp
+			on accession.id = event_link_rlshp.accession_id 
 
 	left join event
 		on event_link_rlshp.event_id = event.id and event.event_type_id in ('313','1514')
 
-	left join event
-		on event_link_rlshp.event_id = event.id
 
 	left join date
 		on date.event_id = event.id
+
+
 
 where date.begin between '2015-07-01' and '2016-06-30'
 

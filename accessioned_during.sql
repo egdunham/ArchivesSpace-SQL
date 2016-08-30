14b59130dfb265e9d9323eb933144f906e8ca569
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2016-08-29 17:04:56 -0700

    Update accessioned_during.sql

--- accessioned_during.sql
+++ accessioned_during.sql
@@ -1,12 +1,11 @@
 select repository.name, sum(extent.number)
 
 from archivesspace.accession right join archivesspace.extent 
-	on accession.id = extent.accession_id
+	on accession.id = extent.accession_id and extent.extent_type_id = '278'
 
 left join archivesspace.repository
 	on accession.repo_id = repository.id
 
 where accession.accession_date between '2014-07-01' and '2015-06-30'
-and extent.extent_type_id = '278'
 
 group by repository.name

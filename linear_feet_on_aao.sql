eadfcafa6dd64c252b575c73802901e5bd1da175
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2016-08-29 17:06:47 -0700

    Update linear_feet_on_aao.sql

--- linear_feet_on_aao.sql
+++ linear_feet_on_aao.sql
@@ -6,14 +6,12 @@ from accession
 		on accession.id = user_defined.accession_id
 
 	right join archivesspace.extent 
-		on accession.id = extent.accession_id
+		on accession.id = extent.accession_id and extent.extent_type_id = 278
 
 		left join archivesspace.deaccession
 			on accession.id = deaccession.accession_id
 
 	where (text_4 = 'INV_AAO' or text_2 = 'INV_AAO')
-
-		and extent.extent_type_id = 278
 
 		and (deaccession.scope_id is null 
 			or deaccession.scope_id = '923')

e9208f66c6876e6e5ea400477e1ddd0ae588bd01
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2020-04-15 14:24:03 -0700

    Update aao_accession_records_new.sql

--- aao_accession_records_new.sql
+++ aao_accession_records_new.sql
@@ -2,16 +2,17 @@ select *
 	
     from(select
     accession.id,
-    repository.name as repo_name,
+    repository.name,
     accession.title,
-    accession.content_description,
+    accession.content_description as descr,
     MAX(IF(enumeration_value.value like '%linear%', extent.number, NULL)) as lf, 
     MAX(IF(event.event_type_id = 313, "TRUE", "FALSE")) as processed_1,
     MAX(IF(event.event_type_id = 1514, "TRUE", "FALSE")) as processed_2,
     MAX(IF(date.date_type_id = 905, date.expression, NULL)) as date,
-    MAX(IF(date.date_type_id = 905, date.begin, NULL)) as date_begin,
-    MAX(IF(date.date_type_id = 905, date.end, NULL)) as date_end,
+    MAX(IF(date.date_type_id = 905, date.begin, NULL)) as begin,
+    MAX(IF(date.date_type_id = 905, date.end, NULL)) as end,
     MAX(IF(user_defined.text_2 like '%INV_AAO%' or user_defined.text_4 like '%INV_AAO%', "TRUE", "FALSE")) as on_aao,
+    MAX(IF(user_defined.text_2 like '%Do not export%' or user_defined.text_4 like '%Do not export%', "TRUE", "FALSE")) as no_publish,
 	accession.identifier as accno,
     user_defined.text_2, 
 	user_defined.text_4,
@@ -44,9 +45,10 @@ select *
 	
     group by accession.id, user_defined.text_2, user_defined.text_4, deaccessioned) as filter_values
     
-    where filter_values.repo_name not in ("Thunderbird School of Global Management", "Architecture & Environmental Design Library")
+    where filter_values.name not in ("Thunderbird School of Global Management", "Architecture & Environmental Design Library")
     
 	and filter_values.on_aao = "FALSE"
     and filter_values.deaccessioned = "FALSE"
     and filter_values.processed_1 = "FALSE"
     and filter_values.processed_2 = "FALSE"
+    and filter_values.no_publish = "FALSE"

c6b9b147782173bb7eb5a9a81099351b86e722d0
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2021-02-08 14:01:14 -0700

    Update processed_during.sql

--- processed_during.sql
+++ processed_during.sql
@@ -1,30 +1,44 @@
 /* You can also search for the event types  */
 
-select 
-	accession.id,
-	repository.name,
-	accession.title,
-	event_type_id, 
-	date.begin, 
-	extent.extent_type_id,
-	extent.number
-
-from accession
-
-	join extent 
-			on accession.id = extent.accession_id and extent_type_id = 278
+select * 
+	
+    from(select
+    accession.id,
+    repository.name,
+    accession.title,
+    MAX(IF(enumeration_value.value like '%linear%', extent.number, NULL)) as lf, 
+    MAX(IF(event.event_type_id = 313, "TRUE", "FALSE")) as processed_1,
+    MAX(IF(event.event_type_id = 1514, "TRUE", "FALSE")) as processed_2,
+    date.begin as start_date,
+    date.end as end_date,
+	accession.identifier as accno,
+    IF(deaccession.scope_id = 922, "TRUE", "FALSE") as deaccessioned	
+	
+    from accession
 
 	left join event_link_rlshp
-			on accession.id = event_link_rlshp.accession_id 
+		on accession.id = event_link_rlshp.accession_id
 
-	left join event
-		on event_link_rlshp.event_id = event.id and event.event_type_id in ('313','1514')
+	left join event on event_link_rlshp.event_id = event.id
 
-	left join date
-		on date.event_id = event.id
+	left join extent
+		on accession.id = extent.accession_id
+	
+	left join enumeration_value 
+		on extent.extent_type_id = enumeration_value.id
 
-	join repository on accession.repo_id = repository.id
-
-	where date.begin between '2015-07-01' and '2016-06-30'
-
-group by accession.id
+	left join repository
+		on accession.repo_id = repository.id
+	
+    left join user_defined
+		on accession.id = user_defined.accession_id
+	
+    left join archivesspace.deaccession
+		on accession.id = deaccession.accession_id
+	
+    left join archivesspace.date
+		on event.id = date.event_id
+	
+    group by accession.id, date.begin, date.end, deaccessioned) as filter_values
+    
+    where (processed_1 = "TRUE" or processed_2 = "TRUE") and start_date between '2020-01-01' and '2020-12-30'

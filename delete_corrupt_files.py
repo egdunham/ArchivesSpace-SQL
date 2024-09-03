4cccc64b161bedce2570dcb55f863d3f15301f23
egdunham4720 <egdunham@asu.edu>
2024-09-02 18:04:16 -0700

    Add functionality for deleting corrupt files based on filetype

--- delete_corrupt_files.py
+++ delete_corrupt_files.py
@@ -1,5 +1,24 @@
-import os.path
+import os
+import csv
+import filetype
 
-result = os.path.ismount('\\libfile.lib.asu.edu\share')
+#result = os.path.ismount('\\libfile.lib.asu.edu\share')
 
-print(result)
+# Add filepaths for the root folder and error CSV
+filepath = r"C:\Users\egdun\OneDrive\Desktop\test_data"
+csv_output = os.path.normpath(r"C:\Users\egdun\OneDrive\Desktop\errors.csv")
+
+with open(csv_output, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(["filepath"])
+
+    for (root,dirs,files) in os.walk(filepath, topdown=True):
+        for file in files:
+                filepath = os.path.join(root, file)
+                kind = filetype.guess(filepath)
+
+
+                if kind is None:
+                        row = [filepath]
+                        csvwriter.writerow(row)
+                        os.remove(filepath)
